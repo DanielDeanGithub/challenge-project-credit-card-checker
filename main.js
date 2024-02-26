c22547714789be645df0f4e4744dd890d06face1
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-26 11:40:19 +0000

    Push invalid cards to invalidCards array

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ const findInvalidCards = arr => {
     for(let i = 0; i < arr.length; i++) {
         for(let j = 0; j < arr[i].length; j++) {
             if(!validateCred(arr[i][j])) {
-                
+                invalidCards.push(arr[i][j]);
             }
         }
     }
